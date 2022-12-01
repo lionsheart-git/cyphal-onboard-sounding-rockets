66006bfaa3f00d2ba41851fd1b4c3f974bdd0376
Patrick Gloeckner <p.gloeckner@tu-braunschweig.de>
2022-12-01 11:39:42 +0100

    Functions for creating a can socket

--- src/SocketCAN/SocketCANTransceiver.cpp
+++ src/SocketCAN/SocketCANTransceiver.cpp
@@ -4,4 +4,37 @@
  * @date 01.12.22
 */
 
+#include <string>
 #include "SocketCANTransceiver.h"
+
+SocketCANTransceiver::SocketCANTransceiver(std::string ifrName)
+: socket_(0) {
+    CreateSocket();
+    BindSocket(IfrIndex(ifrName));
+}
+void SocketCANTransceiver::CreateSocket() {
+    if ((socket_ = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) {
+        perror("Socket");
+        //@todo Error handling
+    }
+}
+
+void SocketCANTransceiver::BindSocket(struct sockaddr_can addr) {
+    if (bind(socket_, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        perror("Bind");
+        //@todo Error handling
+    }
+}
+
+struct sockaddr_can SocketCANTransceiver::IfrIndex(std::string ifrName) {
+    struct ifreq ifr;
+    strcpy(ifr.ifr_name, ifrName.c_str());
+    ioctl(socket_, SIOCGIFINDEX, &ifr);
+
+    struct sockaddr_can addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.can_family = AF_CAN;
+    addr.can_ifindex = ifr.ifr_ifindex;
+
+    return addr;
+}
--- src/SocketCAN/SocketCANTransceiver.h
+++ src/SocketCAN/SocketCANTransceiver.h
@@ -7,7 +7,28 @@
 #ifndef SOCKETCAN__SOCKETCANTRANSCEIVER_H_
 #define SOCKETCAN__SOCKETCANTRANSCEIVER_H_
 
+#include <cstdio>
+#include <cstring>
+#include <unistd.h>
+
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+
+#include <linux/can.h>
+#include <linux/can/raw.h>
+
 class SocketCANTransceiver {
+  public:
+    explicit SocketCANTransceiver(std::string ifrName);
+
+  private:
+    void CreateSocket();
+    void BindSocket(struct sockaddr_can addr);
+    struct sockaddr_can IfrIndex(std::string ifrName);
+
+
+    int socket_;
 
 };
 
