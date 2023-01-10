be30a4afbc7d9e1bb88a9fa96f831a29c247056a
Patrick Gloeckner <p.gloeckner@tu-braunschweig.de>
2023-01-10 15:20:34 +0100

    Restructuring of main class

--- src/App/main.cpp
+++ src/App/main.cpp
@@ -34,10 +34,138 @@
 // Function prototypes
 static void *memAllocate(CanardInstance *const ins, const size_t amount);
 static void memFree(CanardInstance *const ins, void *const pointer);
+static void getUniqueID(uint8_t out[uavcan_node_GetInfo_Response_1_0_unique_id_ARRAY_CAPACITY_]);
+static uavcan_node_GetInfo_Response_1_0 processRequestNodeGetInfo();
+static CanardMicrosecond getMonotonicMicroseconds();
+static void sendResponse(CanardTxQueue *tx_queue,
+                         CanardInstance *ins,
+                         const CanardRxTransfer *const original_request_transfer,
+                         const size_t payload_size,
+                         const void *const payload);
 static void processReceivedTransfer(CanardTxQueue *tx_queue,
                                     CanardInstance *ins,
                                     SocketCANTransceiver *const transceiver,
                                     const CanardRxTransfer *const transfer);
+static void handle1HzLoop(CanardInstance &canard,
+                          CanardTxQueue &tx_queue,
+                          O1HeapInstance const *heap,
+                          const CanardMicrosecond monotonic_time,
+                          CanardMicrosecond const started_at);
+
+int main() {
+
+    // Create an o1heap and Canard instance
+    O1HeapInstance *my_allocator;
+    alignas(O1HEAP_ALIGNMENT) static uint8_t heap_arena[O1HEAP_MEM_SIZE] = {0};
+    my_allocator = o1heapInit(heap_arena, sizeof(heap_arena));
+
+    // Initialize canard as CANFD and node no. 96
+    CanardInstance ins;
+    CanardTxQueue txQueue;
+    ins = canardInit(&memAllocate, &memFree);
+    ins.user_reference = my_allocator;
+    ins.node_id = NODE_ID;
+
+    txQueue = canardTxInit(100, CANARD_MTU_CAN_FD);
+
+    // Init SocketCanTransceiver
+    SocketCANTransceiver transceiver("vcan0", true);
+
+    // Subscribe to GetInfo requests
+    static CanardRxSubscription rx;
+    const int8_t res =  //
+        canardRxSubscribe(&ins,
+                          CanardTransferKindRequest,
+                          uavcan_node_GetInfo_1_0_FIXED_PORT_ID_,
+                          uavcan_node_GetInfo_Request_1_0_EXTENT_BYTES_,
+                          CANARD_DEFAULT_TRANSFER_ID_TIMEOUT_USEC,
+                          &rx);
+    if (res < 0) {
+        return -res;
+    }
+
+    // Now the node is initialized and we're ready to roll.
+    auto started_at = getMonotonicMicroseconds();
+    const CanardMicrosecond fast_loop_period = MEGA / 50;
+    CanardMicrosecond next_fast_iter_at = started_at + fast_loop_period;
+    CanardMicrosecond next_1_hz_iter_at = started_at + MEGA;
+    CanardMicrosecond next_01_hz_iter_at = started_at + MEGA * 10;
+
+    while (true) {
+
+        // Run a trivial scheduler polling the loops that run the business logic.
+        CanardMicrosecond monotonic_time = getMonotonicMicroseconds();
+//    if (monotonic_time >= next_fast_iter_at) {
+//        next_fast_iter_at += fast_loop_period;
+//        handleFastLoop(&state, monotonic_time);
+//    }
+        if (monotonic_time >= next_1_hz_iter_at) {
+            next_1_hz_iter_at += MEGA;
+            handle1HzLoop(ins, txQueue, my_allocator, monotonic_time, started_at);
+        }
+//    if (monotonic_time >= next_01_hz_iter_at) {
+//        next_01_hz_iter_at += MEGA * 10;
+//        handle01HzLoop(&state, monotonic_time);
+//    }
+
+        // Manage CAN RX/TX per redundant interface.
+        for (uint8_t ifidx = 0; ifidx < CAN_REDUNDANCY_FACTOR; ifidx++) {
+            // Transmit pending frames from the prioritized TX queues managed by libcanard.
+            CanardTxQueue *const que = &txQueue;
+            const CanardTxQueueItem *tqi = canardTxPeek(que);  // Find the highest-priority frame.
+            while (tqi != nullptr) {
+                // Attempt transmission only if the frame is not yet timed out while waiting in the TX queue.
+                // Otherwise just drop it and move on to the next one.
+                if ((tqi->tx_deadline_usec == 0) || (tqi->tx_deadline_usec > monotonic_time)) {
+                    // Send fame non-blocking.
+                    const int16_t result = transceiver.SendCanardFrame(tqi->frame, 0);
+                    if (result == 0) {
+                        break;  // The queue is full, we will try again on the next iteration.
+                    }
+                    if (result < 0) {
+                        return -result;  // SocketCAN interface failure (link down?)
+                    }
+                }
+                ins.memory_free(&ins, canardTxPop(que, tqi));
+                tqi = canardTxPeek(que);
+            }
+
+            // Process received frames by feeding them from SocketCAN to libcanard.
+            // The order in which we handle the redundant interfaces doesn't matter -- libcanard can accept incoming
+            // frames from any of the redundant interface in an arbitrary order.
+            // The internal state machine will sort them out and remove duplicates automatically.
+            CanardFrame frame = {0};
+            uint8_t buf[CANARD_MTU_CAN_FD] = {0};
+            const int16_t socketcan_result = socketcanPop(3, &frame, NULL, sizeof(buf), buf, 0, NULL);
+            if (socketcan_result == 0)  // The read operation has timed out with no frames, nothing to do here.
+            {
+                break;
+            }
+            if (socketcan_result < 0)  // The read operation has failed. This is not a normal condition.
+            {
+                return -socketcan_result;
+            }
+            // The SocketCAN adapter uses the wall clock for timestamping, but we need monotonic.
+            // Wall clock can only be used for time synchronization.
+            const CanardMicrosecond timestamp_usec = getMonotonicMicroseconds();
+            CanardRxTransfer transfer = {static_cast<CanardPriority>(0)};
+            const int8_t canard_result = canardRxAccept(&ins, timestamp_usec, &frame, ifidx, &transfer, NULL);
+            if (canard_result > 0) {
+                processReceivedTransfer(&txQueue, &ins, &transceiver, &transfer);
+                ins.memory_free(&ins, (void *) transfer.payload);
+            } else if ((canard_result == 0) || (canard_result == -CANARD_ERROR_OUT_OF_MEMORY)) {
+                (void) 0;  // The frame did not complete a transfer so there is nothing to do.
+                // OOM should never occur if the heap is sized correctly. You can track OOM errors via heap API.
+            } else {
+                assert(false);  // No other error can possibly occur at runtime.
+            }
+        }
+
+        // Run every 5ms to prevent using too much CPU.
+        usleep(TX_PROC_SLEEP_TIME);
+
+    }
+}
 
 // Returns the 128-bit unique-ID of the local node. This value is used in uavcan.node.GetInfo.Response and during the
 // plug-and-play node-ID allocation by uavcan.pnp.NodeIDAllocationData. The function is infallible.
@@ -187,131 +315,6 @@ static void handle1HzLoop(CanardInstance &canard,
                      &serialized[0]);
             }
         }
-    }
-}
-
-int main() {
-
-    // Create an o1heap and Canard instance
-    O1HeapInstance *my_allocator;
-    alignas(O1HEAP_ALIGNMENT) static uint8_t heap_arena[O1HEAP_MEM_SIZE] = {0};
-    my_allocator = o1heapInit(heap_arena, sizeof(heap_arena));
-
-    // Transfer ID
-    static uint8_t my_message_transfer_id = 0;
-
-    // Uptime counter for heartbeat message
-    uint32_t test_uptimeSec = 0;
-
-    // Buffer for serialization of a heartbeat message
-    size_t hbeat_ser_buf_size = uavcan_node_Heartbeat_1_0_EXTENT_BYTES_;
-    uint8_t hbeat_ser_buf[uavcan_node_Heartbeat_1_0_EXTENT_BYTES_];
-
-    // Initialize canard as CANFD and node no. 96
-    CanardInstance ins;
-    CanardTxQueue txQueue;
-    ins = canardInit(&memAllocate, &memFree);
-    ins.user_reference = my_allocator;
-    ins.node_id = NODE_ID;
-
-    txQueue = canardTxInit(100, CANARD_MTU_CAN_FD);
-
-    // Init SocketCanTransceiver
-    SocketCANTransceiver transceiver("vcan0", true);
-
-    // Subscribe to GetInfo requests
-    static CanardRxSubscription rx;
-    const int8_t res =  //
-        canardRxSubscribe(&ins,
-                          CanardTransferKindRequest,
-                          uavcan_node_GetInfo_1_0_FIXED_PORT_ID_,
-                          uavcan_node_GetInfo_Request_1_0_EXTENT_BYTES_,
-                          CANARD_DEFAULT_TRANSFER_ID_TIMEOUT_USEC,
-                          &rx);
-    if (res < 0) {
-        return -res;
-    }
-
-    // Now the node is initialized and we're ready to roll.
-    auto started_at = getMonotonicMicroseconds();
-    const CanardMicrosecond fast_loop_period = MEGA / 50;
-    CanardMicrosecond next_fast_iter_at = started_at + fast_loop_period;
-    CanardMicrosecond next_1_hz_iter_at = started_at + MEGA;
-    CanardMicrosecond next_01_hz_iter_at = started_at + MEGA * 10;
-
-    while (true) {
-
-        // Run a trivial scheduler polling the loops that run the business logic.
-        CanardMicrosecond monotonic_time = getMonotonicMicroseconds();
-//    if (monotonic_time >= next_fast_iter_at) {
-//        next_fast_iter_at += fast_loop_period;
-//        handleFastLoop(&state, monotonic_time);
-//    }
-        if (monotonic_time >= next_1_hz_iter_at) {
-            next_1_hz_iter_at += MEGA;
-            handle1HzLoop(ins, txQueue, my_allocator, monotonic_time, started_at);
-        }
-//    if (monotonic_time >= next_01_hz_iter_at) {
-//        next_01_hz_iter_at += MEGA * 10;
-//        handle01HzLoop(&state, monotonic_time);
-//    }
-
-        // Manage CAN RX/TX per redundant interface.
-        for (uint8_t ifidx = 0; ifidx < CAN_REDUNDANCY_FACTOR; ifidx++) {
-            // Transmit pending frames from the prioritized TX queues managed by libcanard.
-            CanardTxQueue *const que = &txQueue;
-            const CanardTxQueueItem *tqi = canardTxPeek(que);  // Find the highest-priority frame.
-            while (tqi != nullptr) {
-                // Attempt transmission only if the frame is not yet timed out while waiting in the TX queue.
-                // Otherwise just drop it and move on to the next one.
-                if ((tqi->tx_deadline_usec == 0) || (tqi->tx_deadline_usec > monotonic_time)) {
-                    // Send fame non-blocking.
-                    const int16_t result = transceiver.SendCanardFrame(tqi->frame, 0);
-                    if (result == 0) {
-                        break;  // The queue is full, we will try again on the next iteration.
-                    }
-                    if (result < 0) {
-                        return -result;  // SocketCAN interface failure (link down?)
-                    }
-                }
-                ins.memory_free(&ins, canardTxPop(que, tqi));
-                tqi = canardTxPeek(que);
-            }
-
-            // Process received frames by feeding them from SocketCAN to libcanard.
-            // The order in which we handle the redundant interfaces doesn't matter -- libcanard can accept incoming
-            // frames from any of the redundant interface in an arbitrary order.
-            // The internal state machine will sort them out and remove duplicates automatically.
-            CanardFrame frame = {0};
-            uint8_t buf[CANARD_MTU_CAN_FD] = {0};
-            const int16_t socketcan_result = socketcanPop(3, &frame, NULL, sizeof(buf), buf, 0, NULL);
-            if (socketcan_result == 0)  // The read operation has timed out with no frames, nothing to do here.
-            {
-                break;
-            }
-            if (socketcan_result < 0)  // The read operation has failed. This is not a normal condition.
-            {
-                return -socketcan_result;
-            }
-            // The SocketCAN adapter uses the wall clock for timestamping, but we need monotonic.
-            // Wall clock can only be used for time synchronization.
-            const CanardMicrosecond timestamp_usec = getMonotonicMicroseconds();
-            CanardRxTransfer transfer = {static_cast<CanardPriority>(0)};
-            const int8_t canard_result = canardRxAccept(&ins, timestamp_usec, &frame, ifidx, &transfer, NULL);
-            if (canard_result > 0) {
-                processReceivedTransfer(&txQueue, &ins, &transceiver, &transfer);
-                ins.memory_free(&ins, (void *) transfer.payload);
-            } else if ((canard_result == 0) || (canard_result == -CANARD_ERROR_OUT_OF_MEMORY)) {
-                (void) 0;  // The frame did not complete a transfer so there is nothing to do.
-                // OOM should never occur if the heap is sized correctly. You can track OOM errors via heap API.
-            } else {
-                assert(false);  // No other error can possibly occur at runtime.
-            }
-        }
-
-        // Run every 5ms to prevent using too much CPU.
-        usleep(TX_PROC_SLEEP_TIME);
-
     }
 }
 
