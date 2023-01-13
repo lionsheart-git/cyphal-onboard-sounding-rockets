9433453068cd7e91b7438cc974050a52800f42c6
Patrick Gloeckner <p.gloeckner@tu-braunschweig.de>
2023-01-13 11:32:46 +0100

    Definition of the constructor

--- src/OpenCyphal/OpenCyphal.cpp
+++ src/OpenCyphal/OpenCyphal.cpp
@@ -5,3 +5,13 @@
 */
 
 #include "OpenCyphal.h"
+
+OpenCyphal::OpenCyphal(CanardTransceiver &transceiver)
+    : o1heap_allocator_(o1heapInit(heap_arena, sizeof(heap_arena))),
+    instance_(canardInit(&memAllocate, &memFree)) {
+    instance_.user_reference = o1heap_allocator_;
+    instance_.node_id = NODE_ID;
+
+    transceiver_[0] = &transceiver;
+    tx_queues_[0] = canardTxInit(100, CANARD_MTU_CAN_FD);
+}
--- src/OpenCyphal/OpenCyphal.h
+++ src/OpenCyphal/OpenCyphal.h
@@ -9,6 +9,9 @@
 
 #define CAN_REDUNDANCY_FACTOR 1
 #define O1HEAP_MEM_SIZE 4096
+#define NODE_ID 96
+
+#include <cassert>
 
 #include <canard.h>
 #include <o1heap.h>
@@ -41,6 +44,18 @@ class OpenCyphal {
     // O1Heap variables
     O1HeapInstance *o1heap_allocator_;
     alignas(O1HEAP_ALIGNMENT) uint8_t heap_arena[O1HEAP_MEM_SIZE] = {0};
+
+    /* Standard memAllocate and memFree from o1heap examples. */
+    static void *memAllocate(CanardInstance *const ins, const size_t amount) {
+        O1HeapInstance *const heap = (O1HeapInstance *) ins->user_reference;
+        assert(o1heapDoInvariantsHold(heap));
+        return o1heapAllocate(heap, amount);
+    }
+
+    static void memFree(CanardInstance *const ins, void *const pointer) {
+        O1HeapInstance *const heap = (O1HeapInstance *) ins->user_reference;
+        o1heapFree(heap, pointer);
+    }
 
 };
 
