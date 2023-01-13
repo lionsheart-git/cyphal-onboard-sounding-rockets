cf9a087f635db8cead0711c107242d08e46e9bfd
Patrick Gloeckner <p.gloeckner@tu-braunschweig.de>
2023-01-13 18:23:24 +0100

    Function for handling the TX and RX Queues

--- src/OpenCyphal/OpenCyphal.cpp
+++ src/OpenCyphal/OpenCyphal.cpp
@@ -64,3 +64,101 @@ void OpenCyphal::addTransceiver(CanardTransceiver &transceiver) {
         }
     }
 }
+
+int32_t OpenCyphal::HandleTxRxQueues() {
+
+    CanardMicrosecond monotonic_time = getMonotonicMicroseconds();
+    for (uint8_t ifidx = 0; ifidx < CAN_REDUNDANCY_FACTOR; ifidx++) {
+        // Transmit pending frames from the prioritized TX queues managed by libcanard.
+        CanardTxQueue *const que = &tx_queues_[ifidx];
+        const CanardTxQueueItem *tqi = canardTxPeek(que);  // Find the highest-priority frame.
+        while (tqi != nullptr) {
+            // Attempt transmission only if the frame is not yet timed out while waiting in the TX queue.
+            // Otherwise just drop it and move on to the next one.
+            if ((tqi->tx_deadline_usec == 0) || (tqi->tx_deadline_usec > monotonic_time)) {
+                // Send fame non-blocking.
+                const int16_t result = transceiver_[ifidx]->SendCanardFrame(tqi->frame, 0);
+                if (result == 0) {
+                    break;  // The queue is full, we will try again on the next iteration.
+                }
+                if (result < 0) {
+                    return -result;  // SocketCAN interface failure (link down?)
+                }
+            }
+            instance_.memory_free(&instance_, canardTxPop(que, tqi));
+            tqi = canardTxPeek(que);
+        }
+
+        // Process received frames by feeding them from SocketCAN to libcanard.
+        // The order in which we handle the redundant interfaces doesn't matter -- libcanard can accept incoming
+        // frames from any of the redundant interface in an arbitrary order.
+        // The internal state machine will sort them out and remove duplicates automatically.
+        CanardFrame frame = {0};
+        uint8_t buf[CANARD_MTU_CAN_FD] = {0};
+        uint64_t out_timestamp_usec;
+        int16_t socketcan_result = transceiver_[ifidx]->ReceiveCanardFrame(0, out_timestamp_usec, frame, buf);
+        if (socketcan_result == 0)  // The read operation has timed out with no frames, nothing to do here.
+        {
+            break;
+        }
+        if (socketcan_result < 0)  // The read operation has failed. This is not a normal condition.
+        {
+            return -socketcan_result;
+        }
+        // The SocketCAN adapter uses the wall clock for timestamping, but we need monotonic.
+        // Wall clock can only be used for time synchronization.
+        const CanardMicrosecond timestamp_usec = getMonotonicMicroseconds();
+        CanardRxTransfer transfer = {static_cast<CanardPriority>(0)};
+        const int8_t canard_result = canardRxAccept(&instance_, timestamp_usec, &frame, ifidx, &transfer, NULL);
+        if (canard_result > 0) {
+            ProcessReceivedTransfer(ifidx, transfer);
+            instance_.memory_free(&instance_, (void *) transfer.payload);
+        } else if ((canard_result == 0) || (canard_result == -CANARD_ERROR_OUT_OF_MEMORY)) {
+            (void) 0;  // The frame did not complete a transfer so there is nothing to do.
+            // OOM should never occur if the heap is sized correctly. You can track OOM errors via heap API.
+        } else {
+            assert(false);  // No other error can possibly occur at runtime.
+        }
+    }
+}
+
+void OpenCyphal::ProcessReceivedTransfer(uint8_t interface_index, CanardRxTransfer const &transfer) {
+    if (transfer.metadata.transfer_kind == CanardTransferKindRequest) {
+        if (transfer.metadata.port_id == uavcan_node_GetInfo_1_0_FIXED_PORT_ID_) {
+            // The request object is empty so we don't bother deserializing it. Just send the response.
+            const uavcan_node_GetInfo_Response_1_0 resp = ProcessRequestNodeGetInfo();
+            uint8_t serialized[uavcan_node_GetInfo_Response_1_0_SERIALIZATION_BUFFER_SIZE_BYTES_] = {0};
+            size_t serialized_size = sizeof(serialized);
+            const int8_t res = uavcan_node_GetInfo_Response_1_0_serialize_(&resp, &serialized[0], &serialized_size);
+            if (res >= 0) {
+                CanardTransferMetadata meta = transfer.metadata;
+                meta.transfer_kind = CanardTransferKindResponse;
+                Publish(transfer.timestamp_usec + MEGA, &meta, serialized_size, serialized, interface_index);
+            } else {
+                assert(false);
+            }
+        }
+    }
+}
+
+uavcan_node_GetInfo_Response_1_0 OpenCyphal::ProcessRequestNodeGetInfo() {
+    uavcan_node_GetInfo_Response_1_0 resp = {0};
+    resp.protocol_version.major = CANARD_CYPHAL_SPECIFICATION_VERSION_MAJOR;
+    resp.protocol_version.minor = CANARD_CYPHAL_SPECIFICATION_VERSION_MINOR;
+
+    // The hardware version is not populated in this demo because it runs on no specific hardware.
+    // An embedded node like a servo would usually determine the version by querying the hardware.
+
+    resp.software_version.major = VERSION_MAJOR;
+    resp.software_version.minor = VERSION_MINOR;
+    resp.software_vcs_revision_id = VCS_REVISION_ID;
+
+    getUniqueID(resp.unique_id);
+
+    // The node name is the name of the product like a reversed Internet domain name (or like a Java package).
+    resp.name.count = strlen(NODE_NAME);
+    memcpy(&resp.name.elements, NODE_NAME, resp.name.count);
+
+    // The software image CRC and the Certificate of Authenticity are optional so not populated in this demo.
+    return resp;
+}
--- src/OpenCyphal/OpenCyphal.h
+++ src/OpenCyphal/OpenCyphal.h
@@ -11,12 +11,20 @@
 #define O1HEAP_MEM_SIZE 4096
 #define NODE_ID 96
 
+#define KILO 1000L
+#define MEGA ((int64_t) KILO * KILO)
+
 #include <cassert>
+#include <ctime>
+#include <cstdlib>
 
 #include <canard.h>
 #include <o1heap.h>
 
+#include "uavcan/node/GetInfo_1_0.h"
+
 #include "CanardTransceiver.h"
+#include "uavcan/_register/Value_1_0.h"
 
 class OpenCyphal {
   public:
@@ -41,6 +49,8 @@ class OpenCyphal {
 
     void addTransceiver(CanardTransceiver &transceiver);
 
+    int32_t HandleTxRxQueues();
+
   private:
     // Canard variables
     CanardTxQueue tx_queues_[CAN_REDUNDANCY_FACTOR];
@@ -50,6 +60,9 @@ class OpenCyphal {
     // O1Heap variables
     O1HeapInstance *o1heap_allocator_;
     alignas(O1HEAP_ALIGNMENT) uint8_t heap_arena[O1HEAP_MEM_SIZE] = {0};
+
+    void ProcessReceivedTransfer(uint8_t interface_index, CanardRxTransfer const & transfer);
+    static uavcan_node_GetInfo_Response_1_0 ProcessRequestNodeGetInfo();
 
     /* Standard memAllocate and memFree from o1heap examples. */
     static void *memAllocate(CanardInstance *const ins, const size_t amount) {
@@ -63,6 +76,30 @@ class OpenCyphal {
         o1heapFree(heap, pointer);
     }
 
+    static CanardMicrosecond getMonotonicMicroseconds() {
+        struct timespec ts{};
+        if (clock_gettime(CLOCK_MONOTONIC, &ts) != 0) {
+            abort();
+        }
+        return (uint64_t) (ts.tv_sec * 1000000 + ts.tv_nsec / 1000);
+    }
+
+    // Returns the 128-bit unique-ID of the local node. This value is used in uavcan.node.GetInfo.Response and during the
+    // plug-and-play node-ID allocation by uavcan.pnp.NodeIDAllocationData. The function is infallible.
+    static void getUniqueID(uint8_t out[uavcan_node_GetInfo_Response_1_0_unique_id_ARRAY_CAPACITY_]) {
+        // A real hardware node would read its unique-ID from some hardware-specific source (typically stored in ROM).
+        // This example is a software-only node, so we store the unique-ID in a (read-only) register instead.
+        uavcan_register_Value_1_0 value = {0};
+        uavcan_register_Value_1_0_select_unstructured_(&value);
+        // Populate the default; it is only used at the first run if there is no such register.
+        for (uint8_t i = 0; i < uavcan_node_GetInfo_Response_1_0_unique_id_ARRAY_CAPACITY_; i++) {
+            value.unstructured.value.elements[value.unstructured.value.count++] = (uint8_t) rand();  // NOLINT
+        }
+        // registerRead("uavcan.node.unique_id", &value);
+        assert(uavcan_register_Value_1_0_is_unstructured_(&value) &&
+            value.unstructured.value.count == uavcan_node_GetInfo_Response_1_0_unique_id_ARRAY_CAPACITY_);
+        memcpy(&out[0], &value.unstructured.value, uavcan_node_GetInfo_Response_1_0_unique_id_ARRAY_CAPACITY_);
+    }
 };
 
 #endif //SOCKETCAN_SRC_OPENCYPHAL_OPENCYPHAL_H_
