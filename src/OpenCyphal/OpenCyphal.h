e7d83784cd5d12e20e97463c93e4be348c4ce5c2
Patrick Gloeckner <p.gloeckner@tu-braunschweig.de>
2023-01-13 11:04:50 +0100

    Definition of variables needed by Cyphal tu run

--- src/OpenCyphal/OpenCyphal.h
+++ src/OpenCyphal/OpenCyphal.h
@@ -7,9 +7,28 @@
 #ifndef SOCKETCAN_SRC_OPENCYPHAL_OPENCYPHAL_H_
 #define SOCKETCAN_SRC_OPENCYPHAL_OPENCYPHAL_H_
 
+#define CAN_REDUNDANCY_FACTOR 1
+#define O1HEAP_MEM_SIZE 4096
+
+#include <canard.h>
+#include <o1heap.h>
+
+#include "CanardTransceiver.h"
+
 class OpenCyphal {
   public:
-    void addTransceiver()
+
+
+  private:
+    // Canard variables
+    CanardTxQueue tx_queues_[CAN_REDUNDANCY_FACTOR];
+    CanardTransceiver *transceiver_[CAN_REDUNDANCY_FACTOR];
+    CanardInstance instance_;
+
+    // O1Heap variables
+    O1HeapInstance *o1heap_allocator_;
+    alignas(O1HEAP_ALIGNMENT) uint8_t heap_arena[O1HEAP_MEM_SIZE] = {0};
+
 
 };
 
