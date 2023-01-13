f0e9f836988d9c72d4e8c8203533966a7c75f4ac
Patrick Gloeckner <p.gloeckner@tu-braunschweig.de>
2023-01-13 13:13:57 +0100

    Wrote functions for publishing and subscribing

--- src/OpenCyphal/OpenCyphal.cpp
+++ src/OpenCyphal/OpenCyphal.cpp
@@ -15,3 +15,35 @@ OpenCyphal::OpenCyphal(CanardTransceiver &transceiver)
     transceiver_[0] = &transceiver;
     tx_queues_[0] = canardTxInit(100, CANARD_MTU_CAN_FD);
 }
+
+int8_t OpenCyphal::Subscribe(CanardTransferKind const transfer_kind,
+                             CanardPortID const port_id,
+                             size_t const extent,
+                             CanardMicrosecond const transfer_id_timeout_usec,
+                             CanardRxSubscription *out_subscription) {
+
+    return canardRxSubscribe(&instance_,
+                             transfer_kind,
+                             port_id,
+                             transfer_id_timeout_usec,
+                             CANARD_DEFAULT_TRANSFER_ID_TIMEOUT_USEC,
+                             out_subscription);
+}
+
+int32_t OpenCyphal::Publish(CanardMicrosecond const tx_deadline_usec,
+                            CanardTransferMetadata const *const metadata,
+                            size_t const payload_size,
+                            void const *const payload) {
+    //@todo Figure out how to deal with the failing of a push to one tx queue.
+    int32_t retval = 0;
+    for (uint8_t ifidx = 0; ifidx < CAN_REDUNDANCY_FACTOR; ifidx++) {
+        retval = canardTxPush(&tx_queues_[ifidx],
+                            &instance_,
+                            tx_deadline_usec,
+                            metadata,
+                            payload_size,
+                            payload);
+    }
+
+    return retval;
+}
