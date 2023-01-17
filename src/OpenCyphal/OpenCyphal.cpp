20adfb4537661e91319d4c52b1bd18138b363712
Patrick Gloeckner <p.gloeckner@tu-braunschweig.de>
2023-01-17 16:06:12 +0100

    Fixed o1heap initing

--- src/OpenCyphal/OpenCyphal.cpp
+++ src/OpenCyphal/OpenCyphal.cpp
@@ -7,8 +7,11 @@
 #include "OpenCyphal.h"
 
 OpenCyphal::OpenCyphal(CanardTransceiver &transceiver)
-    : o1heap_allocator_(o1heapInit(heap_arena, sizeof(heap_arena))),
+    : o1heap_allocator_(),
     instance_(canardInit(&memAllocate, &memFree)) {
+
+    o1heap_allocator_ = o1heapInit(heap_arena, sizeof(heap_arena));
+
     instance_.user_reference = o1heap_allocator_;
     instance_.node_id = NODE_ID;
 
