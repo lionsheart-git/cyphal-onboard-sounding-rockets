2673130694573d3762705fe8b236118db7ce1336
Patrick Gloeckner <p.gloeckner@tu-braunschweig.de>
2023-01-18 16:29:03 +0100

    Basic documentation

--- src/OpenCyphal/Clock.h
+++ src/OpenCyphal/Clock.h
@@ -11,6 +11,13 @@
 
 class Clock {
   public:
+
+    /// A deeply embedded system should sample a microsecond-resolution non-overflowing 64-bit timer.
+    /// Here is a simple non-blocking implementation as an example:
+    /// https://github.com/PX4/sapog/blob/601f4580b71c3c4da65cc52237e62a/firmware/src/motor/realtime/motor_timer.c#L233-L274
+    /// Mind the difference between monotonic time and wall time. Monotonic time never changes rate or makes leaps,
+    /// it is therefore impossible to synchronize with an external reference. Wall time can be synchronized and therefore
+    /// it may change rate or make leap adjustments. The two kinds of time serve completely different purposes.
     static uint64_t GetMonotonicMicroseconds();
 
 };
