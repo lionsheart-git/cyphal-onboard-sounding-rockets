cab15d2fd13895954e5873c061d7d67796102bc8
Patrick Gloeckner <p.gloeckner@tu-braunschweig.de>
2023-01-19 14:40:24 +0100

    Documented the PMessage class

--- src/OpenCyphal/PMessage.h
+++ src/OpenCyphal/PMessage.h
@@ -9,10 +9,32 @@
 
 #include "canard.h"
 
+/**
+ * @brief Interface for all publish messages.
+ *
+ * All messages that are published should inherit from this base class.
+ */
 class PMessage {
   public:
+    /**
+     * @brief Metadata needed to transfer the payload.
+     *
+     * @return The metadata for the transfer.
+     */
     virtual CanardTransferMetadata Metadata() = 0;
+
+    /**
+     * @briefThe serialized message to be send.
+     *
+     * @return A pointer to the array storing the serialized message.
+     */
     virtual uint8_t *SerializedMessage() = 0;
+
+    /**
+     * @brief Size of the serialized message.
+     *
+     * @return The size of the buffer holding the serialized message.
+     */
     virtual size_t SerializedSize() const = 0;
 
 };
