a250e589b0274e2ceae0dfcb05a036a698a623ba
Patrick Gloeckner <p.gloeckner@tu-braunschweig.de>
2023-01-23 11:36:21 +0100

    Subscribe message to topic get info.

---
+++ src/OpenCyphal/SMessageGetInfo.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file ${FILE}
+ * @author Patrick Glöckner
+ * @date 23.01.23
+*/
+
+#include "SMessageGetInfo.h"
+
+#include "uavcan/node/GetInfo_1_0.h"
+
+SMessageGetInfo::SMessageGetInfo()
+: SMessage(CanardTransferKindRequest,
+           uavcan_node_GetInfo_1_0_FIXED_PORT_ID_,
+           uavcan_node_GetInfo_Request_1_0_EXTENT_BYTES_,
+           CANARD_DEFAULT_TRANSFER_ID_TIMEOUT_USEC)
+{
+}
---
+++ src/OpenCyphal/SMessageGetInfo.h
@@ -0,0 +1,19 @@
+/**
+ * @file ${FILE}
+ * @author Patrick Glöckner
+ * @date 23.01.23
+*/
+
+#ifndef SOCKETCAN_SRC_OPENCYPHAL_SMESSAGEGETINFO_H_
+#define SOCKETCAN_SRC_OPENCYPHAL_SMESSAGEGETINFO_H_
+
+#include "SMessage.h"
+
+class SMessageGetInfo : public SMessage {
+
+  public:
+    SMessageGetInfo();
+
+};
+
+#endif //SOCKETCAN_SRC_OPENCYPHAL_SMESSAGEGETINFO_H_
