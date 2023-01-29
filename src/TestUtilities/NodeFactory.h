fd5e8e407804e8bef6440fa642cced544847ac30
Patrick Gloeckner <p.gloeckner@tu-braunschweig.de>
2023-01-29 10:42:51 +0100

    Added documentation to NodeFactory.h

--- src/TestUtilities/NodeFactory.h
+++ src/TestUtilities/NodeFactory.h
@@ -15,20 +15,52 @@
 #include "CanardTransceiver.h"
 #include "Macros.h"
 
+/**
+ * @class NodeFactory
+ * @brief Factory for creating nodes.
+ */
 class NodeFactory {
   public:
+    /**
+     * @brief Creates a new node factory.
+     *
+     * @param used_ids Array of ids already used.
+     * @param size Size of the array.
+     */
     NodeFactory(uint8_t *used_ids, size_t size);
 
+    /**
+     * @brief Creates a new node with a random id.
+     *
+     * @return Pointer to the new node.
+     */
     Node * CreateNode();
+
+    /**
+     * @brief Creates a new node with the desired id.
+     *
+     * @param node_id ID of the new node to create.
+     * @return Pointer to the new node.
+     */
     Node * CreateNode(uint8_t node_id);
 
+    /**
+     * @brief Adds a new Canard transceiver to the list.
+     *
+     * @param transceiver The new transceiver to add.
+     */
     void AddTransceiver(CanardTransceiver &transceiver);
 
   private:
+    /**
+     * @brief Creates a 128bit unique id.
+     *
+     * @param out Array to populate.
+     */
     static void GetUniqueID(uint8_t out[16U]);
 
-    std::vector<uint8_t> used_ids_;
-    CanardTransceiver *transceiver_[CAN_REDUNDANCY_FACTOR]{};
+    std::vector<uint8_t> used_ids_; /**< List of already used node ids. */
+    CanardTransceiver *transceiver_[CAN_REDUNDANCY_FACTOR]{}; /**< Array of transceivers */
 };
 
 #endif //SOCKETCAN_SRC_TESTUTILITIES_NODEFACTORY_H_
