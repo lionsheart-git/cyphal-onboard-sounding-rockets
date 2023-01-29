12575b3ed796654b267e3fd2de2181c10a3320a4
Patrick Gloeckner <p.gloeckner@tu-braunschweig.de>
2023-01-29 12:50:01 +0100

    Documentation of the byte array task.

--- src/OpenCyphal/TByteArray.h
+++ src/OpenCyphal/TByteArray.h
@@ -10,23 +10,41 @@
 #include "Task.h"
 #include "PMessageByteArray.h"
 
+/**
+ * @class TByteArray
+ * @brief Creates a new task sending a byte array.
+ */
 class TByteArray : public Task {
 
   public:
+    /**
+     * @brief A new task sending a byte array periodically.
+     *
+     * @param port_id ID of the topic of the byte array.
+     * @param data Array containing data to send.
+     * @param size Size of the array.
+     * @param interval[us] Interval of how often to send the array.
+     */
     TByteArray(CanardPortID port_id,
                int8_t *data,
                size_t size,
                uint64_t interval);
 
+    /**
+     * @copydoc Task::Interval
+     */
     uint64_t Interval() const override;
 
+    /**
+     * @copydoc Task::Execute
+     */
     void Execute(Node &node, uint64_t current_time) override;
 
   private:
-    PMessageByteArray message_;
-    uint64_t interval_;
-    size_t size_;
-    int8_t data_[uavcan_primitive_array_Integer8_1_0_SERIALIZATION_BUFFER_SIZE_BYTES_];
+    PMessageByteArray message_; /**< Byte array message to send. */
+    uint64_t interval_; /**< Sending interval. */
+    size_t size_; /**< Size of the data. */
+    int8_t data_[uavcan_primitive_array_Integer8_1_0_SERIALIZATION_BUFFER_SIZE_BYTES_]; /**< Array containing data to send. */
 };
 
 #endif //SOCKETCAN_SRC_OPENCYPHAL_TBYTEARRAY_H_
