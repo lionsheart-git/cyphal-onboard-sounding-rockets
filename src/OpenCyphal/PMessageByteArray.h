3495c43fc2b1d458b33e95737a16ec8d8ddd4d01
Patrick Gloeckner <p.gloeckner@tu-braunschweig.de>
2023-01-29 12:34:03 +0100

    Documentation of the p message byte array.

--- src/OpenCyphal/PMessageByteArray.h
+++ src/OpenCyphal/PMessageByteArray.h
@@ -12,26 +12,61 @@
 
 #include "IPMessage.h"
 
-
+/**
+ * @class PMessageByteArray
+ * @brief Publish message sending a byte array.
+ */
 class PMessageByteArray : public IPMessage {
 
   public:
 
+    /**
+     * @brief New byte array with target port id.
+     *
+     * @param port_id Port id for the topic of the byte array.
+     */
     explicit PMessageByteArray(CanardPortID port_id);
 
+    /**
+     * @brief New byte array with the transfers metadata.
+     *
+     * @param metadata Metadata of the transfer.
+     */
     explicit PMessageByteArray(CanardTransferMetadata metadata);
 
+    /**
+     * @brief Metadata of the transfer.
+     *
+     * @return The populated CanardTransferMetadata struct.
+     */
     CanardTransferMetadata Metadata() override;
 
+    /**
+     * @brief The serialized message.
+     *
+     * @return A pointer to the array containing the serialized message.
+     */
     uint8_t* SerializedMessage() override;
 
+    /**
+     * @brief Size of the serialized message.
+     *
+     * @return The size of the serialized message.
+     */
     size_t SerializedSize() const override;
 
+    /**
+     * @brief Serializes a new message.
+     *
+     * @param byte_array The byte array to serialize.
+     * @param size Size of the input array.
+     * @return Error of the serialization.
+     */
     uint8_t SerializeMessage(int8_t const *byte_array, size_t size);
 
   private:
-    uint8_t serialized_[uavcan_primitive_array_Integer8_1_0_SERIALIZATION_BUFFER_SIZE_BYTES_]{};
-    CanardTransferMetadata metadata_;
+    uint8_t serialized_[uavcan_primitive_array_Integer8_1_0_SERIALIZATION_BUFFER_SIZE_BYTES_]{}; /**< Array containing the serialized message. */
+    CanardTransferMetadata metadata_; /**< Metadata of the transfer. */
 };
 
 #endif //SOCKETCAN_SRC_OPENCYPHAL_PMESSAGEBYTEARRAY_H_
