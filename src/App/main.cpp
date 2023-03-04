eff6b34da44e85e017eb868c102aadd8b1094e42
Patrick Gloeckner <p.gloeckner@tu-braunschweig.de>
2023-03-04 13:39:38 +0100

    Removed no longer needed functions

--- src/App/main.cpp
+++ src/App/main.cpp
@@ -21,23 +21,6 @@
 
 #include "uavcan/node/GetInfo_1_0.h"
 
-// Returns the 128-bit unique-ID of the local node. This value is used in uavcan.node.GetInfo.Response and during the
-// plug-and-play node-ID allocation by uavcan.pnp.NodeIDAllocationData. The function is infallible.
-static void getUniqueID(uint8_t out[uavcan_node_GetInfo_Response_1_0_unique_id_ARRAY_CAPACITY_]) {
-    // A real hardware node would read its unique-ID from some hardware-specific source (typically stored in ROM).
-    // This example is a software-only node, so we store the unique-ID in a (read-only) register instead.
-    uavcan_register_Value_1_0 value = {0};
-    uavcan_register_Value_1_0_select_unstructured_(&value);
-    // Populate the default; it is only used at the first run if there is no such register.
-    for (uint8_t i = 0; i < uavcan_node_GetInfo_Response_1_0_unique_id_ARRAY_CAPACITY_; i++) {
-        value.unstructured.value.elements[value.unstructured.value.count++] = (uint8_t) rand();  // NOLINT
-    }
-    // registerRead("uavcan.node.unique_id", &value);
-    assert(uavcan_register_Value_1_0_is_unstructured_(&value) &&
-        value.unstructured.value.count == uavcan_node_GetInfo_Response_1_0_unique_id_ARRAY_CAPACITY_);
-    memcpy(&out[0], &value.unstructured.value, uavcan_node_GetInfo_Response_1_0_unique_id_ARRAY_CAPACITY_);
-}
-
 int main(int argc, char *argv[]) {
     google::InitGoogleLogging(argv[0]);
     FLAGS_alsologtostderr = 1;
@@ -46,9 +29,9 @@ int main(int argc, char *argv[]) {
     NodeFactory factory;
     factory.AddSocketCanInterface("vcan0");
 
-    auto node = factory.CreateLatencyNode(NODE_ID);
+    auto node = factory.CreateLatencyRequestNode(1);
 
-    auto node2 = factory.CreateLatencyNode(67);
+    auto node2 = factory.CreateLatencyResponseNode(2);
 
 //     auto node3 = factory.CreateNode(42);
 //    auto node4 = factory.CreateNode(58);
@@ -63,9 +46,6 @@ int main(int argc, char *argv[]) {
 //
 //    auto byte_array = std::make_unique<TByteArray>(32, random_data, data_size, MEGA);
 //    node->Schedule(std::move(byte_array));
-
-    auto primitive_empty = std::make_unique<TLatencyMeasurement>(LATENCY_MEASUREMENT_PORT_ID, 67, MEGA / 2);
-    node->Schedule(std::move(primitive_empty));
 
     // Now the node is initialized and we're ready to roll.
     auto started_at = Clock::GetMonotonicMicroseconds();
