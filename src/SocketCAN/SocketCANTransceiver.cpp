c2e0dbfea05dc4619776c394a81efe9284ee7b42
Patrick Gloeckner <p.gloeckner@tu-braunschweig.de>
2023-03-14 12:11:34 +0100

    Removed not needed functions

--- src/SocketCAN/SocketCANTransceiver.cpp
+++ src/SocketCAN/SocketCANTransceiver.cpp
@@ -28,47 +28,11 @@ SocketCANTransceiver::~SocketCANTransceiver() {
     }
 }
 
-void SocketCANTransceiver::SendCANFrame(struct can_frame frame) const {
-    ssize_t nbytes = write(socket_, &frame, sizeof(struct can_frame));
-}
-
-void SocketCANTransceiver::SendCANFrame(struct canfd_frame frame) {
-    ssize_t nbytes = write(socket_, &frame, sizeof(struct canfd_frame));
-}
-
-bool SocketCANTransceiver::Send(uint32_t canid, std::vector<uint8_t> data) {
-    struct canfd_frame frame{};
-
-    frame.can_id = canid;
-    if (data.size() > 8) {
-        return false;
-    }
-
-    frame.len = data.size();
-    std::copy(data.begin(), data.end(), frame.data);
-
-    SendCANFrame(frame);
-
-    return true;
-}
-
-uint8_t SocketCANTransceiver::Receive() {
-    struct can_frame frame{};
-
-    int nbytes = read(socket_, &frame, sizeof(struct can_frame));
-
-    if (nbytes < 0) {
-        return 0;
-    }
-
-    return nbytes;
-}
-
 int16_t SocketCANTransceiver::SendCanardFrame(CanardFrame const &frame, uint64_t const &timeout_usec) const {
     return socketcanPush(socket_, &frame, timeout_usec);
 }
 
-//@todo Figure out how to encapsulate payload so the frame can be returned without need for all the pointers.
+// Figure out how to encapsulate payload so the frame can be returned without need for all the pointers.
 int16_t SocketCANTransceiver::ReceiveCanardFrame(uint64_t const &timeout_usec,
                                                  uint64_t &out_timestamp_usec,
                                                  CanardFrame &out_frame,
--- src/SocketCAN/SocketCANTransceiver.h
+++ src/SocketCAN/SocketCANTransceiver.h
@@ -26,7 +26,7 @@ class SocketCANTransceiver : public CanardTransceiver {
   public:
     explicit SocketCANTransceiver(const std::string& ifrName, bool can_fd);
     SocketCANTransceiver(const SocketCANTransceiver& other_transceiver);
-    ~SocketCANTransceiver();
+    ~SocketCANTransceiver() override;
 
     int16_t SendCanardFrame(CanardFrame const &frame, uint64_t const &timeout_usec) const override;
     int16_t ReceiveCanardFrame(uint64_t const &timeout_usec,
@@ -36,13 +36,7 @@ class SocketCANTransceiver : public CanardTransceiver {
                                uint8_t buf[]) const override;
     int16_t CanardFilter(size_t const num_configs, struct CanardFilter const &configs) const override;
 
-    bool Send(uint32_t canid, std::vector<uint8_t> data);
-    uint8_t Receive();
-
   private:
-    void SendCANFrame(struct can_frame frame) const;
-    void SendCANFrame(struct canfd_frame frame);
-
     int socket_;
     std::string ifrName_;
     bool can_fd_;
