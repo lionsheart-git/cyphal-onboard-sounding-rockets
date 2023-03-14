48f629c312cd02434ddb861d802f5c0a690c6268
Patrick Gloeckner <p.gloeckner@tu-braunschweig.de>
2023-03-14 15:39:23 +0100

    Deletion of not needed classes.

--- src/Tests/SocketCanTest.cpp
+++
@@ -1,7 +0,0 @@
-/**
- * @file ${FILE}
- * @author Patrick Glöckner
- * @date 10.02.23
-*/
-
-#include "SocketCanTest.h"
--- src/Tests/Tests.cpp
+++
@@ -1,7 +0,0 @@
-/**
- * @file ${FILE}
- * @author Patrick Glöckner
- * @date 09.02.23
-*/
-
-#include "Tests.h"
